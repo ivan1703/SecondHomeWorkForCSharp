d3634e76ceed7cc48e42a4069b471b7a8984b6e8
Ivan Balashov <ivan.programmer@bk.ru>
2018-11-06 20:32:54 +0400

    First commit projects

---
+++ SecondHomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SecondHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("программа отправляет все отрицательные элементы массива в конец массива");
+            Console.Write("Сколько элементов в массиве вы хотите - ");
+            int n = 0;
+            try
+            {
+                n = Int32.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Возникла ошибка. Нажмите любую клавишу");
+                Console.ReadKey();
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            Console.WriteLine("исходный массив");
+            foreach (int i in arr){
+                arr[i] = rnd.Next(-100, 101);
+                Console.Write(" {0} ", arr[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Итоговый массив");
+            foreach (int i in arr)
+            {
+            }
+            Console.ReadKey();
+        }
+    }
+}
